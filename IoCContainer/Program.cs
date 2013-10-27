fdf4c4cdb47b854ec9239e3be65fd6acff19a27e
Vasil Dininski <dininski@gmail.com>
2013-10-27 13:23:45 +0200

    Initial commit

---
+++ IoCContainer/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VasContainer;
+
+namespace IoCContainer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var ct = new Container();
+            ct.RegisterDependency(typeof(IPowerSource), typeof(PowerOutlet));
+            ct.Resolve<Laptop>().DisplayPower();
+        }
+    }
+
+    public interface IPowerSource
+    {
+        int GetPower();
+    }
+
+    public class Battery : IPowerSource
+    {
+        public int GetPower()
+        {
+            return -1;
+        }
+    }
+
+    public class PowerOutlet : IPowerSource
+    {
+        public int GetPower()
+        {
+            return 100000;
+        }
+    }
+
+    public class Laptop
+    {
+        private IPowerSource source;
+
+        public Laptop(IPowerSource powerSource)
+        {
+            this.source = powerSource;
+        }
+
+        public void DisplayPower()
+        {
+            Console.WriteLine(this.source.GetPower());
+        }
+    }
+}
---
+++ VasContainer/Container.cs
@@ -0,0 +1,98 @@
+﻿
+namespace VasContainer
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Container
+    {
+        private IDictionary<Type, Type> dependecies;
+
+        private ContainerOptions options;
+
+        private const ContainerOptions defaultOptions = ContainerOptions.None;
+
+        public Container()
+            : this(defaultOptions)
+        {
+        }
+
+        public Container(ContainerOptions options)
+        {
+            this.options = options;
+            this.dependecies = new Dictionary<Type, Type>();
+        }
+
+        public void RegisterDependency(Type dependency, Type classToResolve)
+        {
+            this.dependecies.Add(dependency, classToResolve);
+        }
+
+        public T Resolve<T>() where T : class
+        {
+            var classType = typeof(T);
+
+            var constructors = classType
+                .GetConstructors()
+                // create a smarter way of choosing a constructor
+                // maybe using enum flags :-P
+                .OrderByDescending(x => x.GetParameters().Count());
+
+            if (!constructors.Any())
+            {
+                throw new ArgumentException("The class to be resolved does not have any public constructors!");
+            }
+
+            foreach (var constructor in constructors)
+            {
+                // get the constructor parameters
+                var parameters = constructor.GetParameters();
+
+                if (parameters.Length == 0)
+                {
+                    var result = Activator.CreateInstance<T>();
+                    return result;
+                }
+                else
+                {
+                    var parameterObjects = new List<object>();
+
+                    foreach (var parameter in parameters)
+                    {
+                        var parameterType = parameter.ParameterType;
+                        var parameterConstructors = parameterType.GetConstructors();
+
+                        if (this.options.HasFlag(ContainerOptions.UseDefaultValue))
+                        {
+                            if (parameter.HasDefaultValue)
+                            {
+                                // using default value
+                                var res = Convert.ChangeType(parameter.DefaultValue, parameterType);
+                                parameterObjects.Add(res);
+                                continue;
+                            }
+                        }
+
+                        if (parameterType.IsAbstract || parameterType.IsInterface)
+                        {
+                            //TODO: review and check if the binding flags are persisting along the way
+                            var concreteObjectType = this.dependecies[parameterType];
+                            var method =
+                                typeof(Container)
+                                    .GetMethod("Resolve")
+                                    .MakeGenericMethod(concreteObjectType);
+                            var obj = method.Invoke(this, null);
+                            parameterObjects.Add(obj);
+                        }
+                    }
+
+                    var createdObject = (T)Activator.CreateInstance(classType, parameterObjects.ToArray());
+                    return createdObject;
+                }
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
